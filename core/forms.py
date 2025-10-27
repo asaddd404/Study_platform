e404d5491a76d1d84c8807146261360ba0cc0d04
Asad <asadbek08.04.08@gmail.com>
2025-10-27 16:52:04 +0500

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,18 @@
 from django import forms
-from .models import User
+from django.contrib.auth.forms import UserCreationForm # Импортируем базовую форму
+from .models import User # Импортируем нашу модель
 
+# 
+# ВОТ НОВАЯ ФОРМА
+#
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User # Указываем, что наша модель - это core.User
+        fields = ('username', 'email') # Указываем поля для регистрации (можете добавить 'first_name', 'last_name' и т.д.)
+
+#
+# Ваша существующая форма остается ниже
+#
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
