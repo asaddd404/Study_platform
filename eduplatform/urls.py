15da008e292ff211124f3748b2d49f93f905a030
Asad <asadbek08.04.08@gmail.com>
2025-10-27 17:04:45 +0500

    Update urls.py

--- eduplatform/urls.py
+++ eduplatform/urls.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# 
+# 👇 --- ДОБАВЬТЕ ЭТОТ ИМПОРТ --- 👇
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#
+#
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+#
+# 👇 --- ИЗМЕНИТЕ ЭТУ ЧАСТЬ --- 👇
+#
+# Эта строка обслуживает МЕДИА-файлы (загруженные пользователями)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Эта строка будет обслуживать СТАТИЧЕСКИЕ файлы (css, js, images)
+# когда DEBUG=True
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
